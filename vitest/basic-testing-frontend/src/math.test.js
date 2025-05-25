a5239dcd4509bf43eccfc9cbae8b576d7153d49c
Kevin Rangel <krangel107@gmail.com>
2025-05-24 20:26:02 -0500

    Add more test to unit

--- vitest/basic-testing-frontend/src/math.test.js
+++ vitest/basic-testing-frontend/src/math.test.js
@@ -8,9 +8,28 @@ it('Should summarize all number values in an array', () => {
         (prevValue, curValue) => prevValue + curValue,
         0    
     )
-   
+    const result = add(numbers)
+    expect(result).toBe(expectResult)
+})
+
+it('Should yield NaN if a least one invalid number is provided', () => {
+    const inputs = ['invalid', 1]
+
+    const result = add(inputs)
+
+    expect(result).toBeNaN()
+})
+
+
+it('Should yield a correct sum if an array of numeric string values is provided', () => {
+    const numbers = ['1', '2']
+
     const result = add(numbers)
 
+    const expectResult = numbers.reduce(
+        (prevValue, curValue) => +prevValue + +curValue,
+        0    
+    )
 
     expect(result).toBe(expectResult)
 })
