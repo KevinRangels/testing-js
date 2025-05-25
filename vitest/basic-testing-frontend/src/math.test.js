984c48f63f183e5f704b31970b7937ac7dbd409d
Kevin Rangel <krangel107@gmail.com>
2025-05-24 20:10:01 -0500

    improve test with the AAA pattern

--- vitest/basic-testing-frontend/src/math.test.js
+++ vitest/basic-testing-frontend/src/math.test.js
@@ -2,6 +2,15 @@ import { expect, it } from 'vitest'
 import { add } from './math'
 
 it('Should summarize all number values in an array', () => {
-    const result = add([1,2,3])
-    expect(result).toBe(6)
+    // Arrange
+    const numbers = [1,2,3 ]
+    const expectResult = numbers.reduce(
+        (prevValue, curValue) => prevValue + curValue,
+        0    
+    )
+   
+    const result = add(numbers)
+
+
+    expect(result).toBe(expectResult)
 })
